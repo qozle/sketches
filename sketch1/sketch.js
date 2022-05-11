b2c720a62cfa44f69d7fc2939b9e875c693f2c41
maxter_data <mf.qozle@gmail.com>
2022-05-10 21:18:40 -0400

    init

---
+++ sketch1/sketch.js
@@ -0,0 +1,83 @@
+let sketch = function(p) {
+
+	//  GLOBAL STUFF
+	const level = 7;
+	const rates = [];
+
+	var capturer = new CCapture({
+		format: 'png',
+		workersPath: '../ccapture/',
+		framerate: 60,
+		verbose: true,
+		name: 'circles'
+	});
+
+	//  create random rotation rates for each circle
+	for (i = 0; i <= level; i++){
+		rates[i] = [];
+		rates[i]["a"] = p.random(0, 180);
+		rates[i]["b"] = p.random(0, 180);
+	}
+	console.log(rates);
+
+
+	
+	p.setup = function() {
+		p.createCanvas(720, 560);
+		// p.noStroke();
+		p.angleMode(p.DEGREES);
+		// p.noLoop();
+		p.frameRate(60);
+	}
+
+
+	p.draw = function () {
+		//  start capturing.  can't put this in setup, known bug
+		if (p.frameCount === 1) capturer.start();
+
+		p.clear()
+		p.translate(p.width / 2, p.height / 2);
+		p.rotate((p.frameCount  + rates[6]["a"]) % 360);
+		drawCircle(0, 280, level);
+		capturer.capture(document.getElementById("defaultCanvas0"));
+		if (p.frameCount === 300) {
+			capturer.stop();
+			capturer.save();
+			p.noLoop();
+			return;
+		}
+	}
+
+	function drawCircle(x, radius, level) {
+
+		const tt = (126 * level) / 4.0;
+		p.fill(tt);
+		p.ellipse(x, 0, radius * 2, radius * 2);
+		
+		if (level > 1) {
+			level = level - 1;
+			
+			p.push();
+			p.translate(x - radius / 2, 0);
+			p.rotate((p.frameCount + rates[level]["a"]) % 360);
+			drawCircle(0, radius / 2, level);
+			// p.line(0, -radius / 2, 0, radius / 2);
+			p.pop();
+			
+			p.push();
+			p.translate(x + radius / 2, 0);
+			p.rotate((p.frameCount + rates[level]["b"]) % 360);
+			drawCircle(0, radius / 2, level);
+			// p.line(0, -radius / 2, 0, radius / 2);
+			p.pop();
+			
+		}
+
+		
+	}
+
+}
+
+new p5(sketch, 'container');
+
+
