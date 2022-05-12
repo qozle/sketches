bf2de011a3ad0d0d9ad8ff89d50848560bbe93a1
maxter_data <mf.qozle@gmail.com>
2022-05-12 00:41:40 -0400

    attempt at combining perlin vectors with 'averse' behaviour (keep away from others)

--- sketch2/sketch.js
+++ sketch2/sketch.js
@@ -13,13 +13,15 @@ let sketch = function (p) {
 
 
 	class Triangle {
+
 		constructor(posx, posy, width, height) {
 			this.width = p.random(10, width);
 			this.height = p.random(10, height);
-			this.speed = p.random(1, 5);
+			this.speed = p.random() + 1;
 			this.maxSpeed = 8;
-			this.maxWidth = this.height + p.random(-50, 51);
-			this.maxHeight = this.height + p.random(-50, 51);
+			this.maxWidth = this.height + p.random(50);
+			this.maxHeight = this.height + p.random(50);
+			this.perlin = p.random(10000);
 
 
 			
@@ -39,67 +41,86 @@ let sketch = function (p) {
 			this.p3 = p.createVector(this.pos.x, this.pos.y + this.height / 2);
 		}
 
-		update(perlin) {
+		update() {
 
 			//  increase the height and width until they're max
 			if (this.width < this.maxWidth) this.width++;
 			if (this.height < this.maxHeight) this.height++;
 
+			
+			//  add acceleration to velocity
+			this.velocity.add(this.acceleration);
+			//  normalize
+			// this.velocity.normalize();
+			//  add speed
+			// this.velocity.mult(this.speed);
+			//  limit the speed to the max speed
+			this.velocity.limit(this.maxSpeed);
+			//  move the triangle
+			this.pos.add(this.velocity);
+			//  clear out the acceleration because it will be recalculated next frame
+			this.acceleration.mult(0);
+
+
+			//  wrap
+			if (this.pos.x + this.width / 2 >= p.width / 2 || this.pos.x - this.width / 2 <= -p.width / 2) {
+				this.pos.x = -this.pos.x;
+			}
+
+			if (this.pos.y + this.height / 2 >= p.height / 2 || this.pos.y - this.height / 2 <= -p.height / 2) {
+				this.pos.y = -this.pos.y;
+			}
+
+		}
+
+
+		perlinForce() {
 
 			//  each triangle wanders to its own noise
-			p.noiseSeed(perlin);
-			// let randomVector = p5.Vector.fromAngle(p.noiseSeed(p.frameCount) * 360);
-			// randomVector.setMag(p.random(0, this.maxSpeed));
+			p.noiseSeed(this.perlin);
 
 			let randomVector = p.createVector(
-				(((p.noise(p.frameCount / 10) * 2) - 1) * this.speed),
-				(((p.noise(p.frameCount / 10 + 1000) * 2) - 1) * this.speed)
+				(((p.noise(p.frameCount / 25) * 2) - 1) * this.speed),
+				(((p.noise(p.frameCount / 25 + 1000) * 2) - 1) * this.speed)
 			);
 
 			//  add randomVector to acceleration, which should be
 			//  the sum of all force vectors
 			this.acceleration.add(randomVector);
-			//  add acceleration to velocity
-			this.velocity.add(this.acceleration);
-			//  limit the speed to the max speed
-			this.velocity.limit(this.maxSpeed);
-			//  move the triangle
-			this.pos.add(this.velocity);
-			//  clear out the acceleration because it will be
-			//  recalculated next frame
-			this.acceleration.mult(0);
-
-
-			//  wrap
-			if (this.pos.x >= p.width / 2 || this.pos.x <= -p.width / 2) {
-				this.pos.x = -this.pos.x;
-			}
-
-			if (this.pos.y >= p.height / 2 || this.pos.y <= -p.height / 2) {
-				this.pos.y = -this.pos.y;
-			}
-
-
-
-
-			// console.log('randomvector');
-			// console.log(randomVector);
-			// console.log("velocity:");
-			// console.log(this.velocity);
-			// console.log("pos");
-			// console.log(this.pos);
 		}
+
+
+		averse() {
+			
+			triangles.forEach((tri) => {
+				let centerDist = p.dist(tri.pos.x, tri.pos.y, this.pos.x, this.pos.y);
+				let keepDist = tri.width + tri.height + this.width + this.height;
+
+				if (centerDist <= keepDist) {
+
+					//  create a vector pointing away from the point
+					let averseV = p5.Vector.fromAngle(this.pos.angleBetween(tri.pos));
+					averseV.setMag(p.map(centerDist, keepDist, 0, 0, this.maxSpeed));
+		
+					this.acceleration.sub(averseV);
+
+				}
+	
+			});
+		}
+
 
 
 		show() {
 			p.push();
 
-			// p.fill(51);
 			p.noFill();
 			p.stroke(100);
 
+			//  translate to the position of the triangle
 			p.translate(this.pos.x, this.pos.y)
 
+			//  so the upper point is facing the 
 			p.rotate(this.velocity.heading() - 90);
 
 			
@@ -126,6 +147,26 @@ let sketch = function (p) {
 
 			p.line(0, this.height / 2, 0, -this.height / 2)
 
+			// p.fill(255, 0, 0);
+			// p.circle(0, 0, 5)
+			
+			// //p1 BLUE bottom left
+			// p.stroke(0, 0, 255);
+			// p.circle(this.p1.x, this.p1.y, this.height + this.width);
+
+			// //p2 GREEN bottom right
+			// p.stroke(0, 255, 0);
+			// p.circle(this.p2.x, this.p2.y, this.height + this.width);
+
+			// //p3 PURPLE top
+			// p.stroke(255, 0, 255);
+			// p.circle(this.p3.x, this.p3.y, this.height + this.width);
+
+			p.stroke(0, 0, 255);
+			p.circle(0, 0, (this.height + this.width));
+
+
+
 			p.pop();
 		}
   	}	
@@ -136,10 +177,7 @@ let sketch = function (p) {
 	function make_triangle() {
 		if (count < maxCount) {
 			
-			triangles.push({
-				'tri': new Triangle(p.random(p.width / 2), p.random(p.height / 2), p.random(11, 51), p.random(1, 51)),
-				'perlin': p.random(0, 10000)
-			});
+			triangles.push(new Triangle(p.random(p.width / 2), p.random(p.height / 2), 1, 1));
 
 			count++;
 
@@ -155,6 +193,8 @@ let sketch = function (p) {
 	var triangles = [];
 	let count = 0;
 	let maxCount = 50;
+
+
 
 	make_triangle();
 	triangle_interval = setInterval(make_triangle, 5000);	
@@ -190,9 +230,11 @@ let sketch = function (p) {
 		p.translate(p.width / 2, p.height / 2);
 		p.angleMode(p.DEGREES);
 
-		triangles.forEach((tri) => {
-			tri.tri.update(tri.perlin);
-			tri.tri.show();
+		triangles.forEach((triangle) => {
+			triangle.averse();
+			triangle.perlinForce();
+			triangle.update();
+			triangle.show();
 		});
 
 
